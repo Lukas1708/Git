54d8fa72dbfd87d9f717a3b6bdd47e64e8d9db42
Lukas1708 <65906622+Lukas1708@users.noreply.github.com>
2021-05-16 22:53:32 +0200

    Add files via upload
    zadanie Webex

---
+++ podnety.js
@@ -0,0 +1,61 @@
+<body>
+
+<h2 align="center">Pridávanie podnetov do aplikácie</h2><br>
+</br>
+Meno a priezvisko: <td><input type='text' name='meno' value='' size='30' required></td><br>
+</br>
+
+Adresa: <td><input type='text' name='meno' value='' size='30' required></td><br></br>
+<div   id="idSendCV38" class="form-group err-email_body" ><label for="idSendCVemailxbody"  class="control-label col-xs-12 col-sm-3" >Popis podnetu:</label><div class='col-xs-10 col-sm-6 col-lg-4'><textarea  name="email_body" cols="35" rows="5"  id="idSendCVemailxbody" class="form-control" ></textarea></div> <span class="data-popover form-info " data-toggle="popover" data-placement="top" data-content="Sprievodn&#xFD;&#x20;text&#x20;sl&#xFA;&#x017E;i&#x20;rovnako&#x20;ako&#x20;e-mail&#x20;na&#x20;oslovenie&#x20;spolo&#x010D;nosti.&#x20;Informujete&#x20;v&#x20;&#x0148;om&#x20;adres&#xE1;ta,&#x20;na&#x20;ak&#xFA;&#x20;poz&#xED;ciu&#x20;reagujete&#x20;a&#x20;ak&#xE9;&#x20;dokumenty&#x20;prip&#xE1;jate.&#x20;M&#xF4;&#x017E;ete&#x20;ho&#x20;vyu&#x017E;i&#x0165;&#x20;aj&#x20;namiesto&#x20;motiva&#x010D;n&#xE9;ho&#x20;listu,&#x20;kde&#x20;uvediete,&#x20;pre&#x010D;o&#x20;V&#xE1;s&#x20;pracovn&#xE1;&#x20;ponuka&#x20;zaujala,&#x20;ak&#xE9;&#x20;m&#xE1;te&#x20;s&#x20;danou&#x20;poz&#xED;ciou&#x20;sk&#xFA;senosti,&#x20;na&#x20;akej&#x20;&#xFA;rovni&#x20;ovl&#xE1;date&#x20;po&#x017E;adovan&#xE9;&#x20;zru&#x010D;nosti&#x20;a&#x20;&#x010D;o&#x20;m&#xF4;&#x017E;ete&#x20;spolo&#x010D;nosti&#x20;pon&#xFA;knu&#x0165;.&#x0A;&lt;br&#x20;&#x2F;&gt;&lt;br&#x20;&#x2F;&gt;&#x0A;V&#x0161;etky&#x20;tipy&#x20;k&#x20;tvorbe&#x20;motiva&#x010D;n&#xE9;ho&#x20;listu&#x20;a&#x20;&#x017E;ivotopisu&#x20;n&#xE1;jdete&#x20;v&#x20;sekcii&#x20;&lt;a&#x20;href&#x3D;&quot;https&#x3A;&#x2F;&#x2F;www.profesia.sk&#x2F;kariera-v-kocke&#x2F;&quot;&#x20;target&#x3D;&quot;_blank&quot;&gt;Kari&#xE9;ra&#x20;v&#x20;kocke&lt;&#x2F;a&gt;." id="idSendCV37"></span><!--~~ email_body ~~--></div> <div   id="tridSendCV10" class="form-group err-" ><label for="idSendCV10"  class="control-label col-xs-12 col-sm-3" ><span class="half-margin-on-right"></span>
+
+<head>
+
+</head>
+<body>
+    <form action="upload-manager.php" method="post" enctype="multipart/form-data">
+</br>
+Obrázok:
+        <label "for="fileSelect" ></label>
+        <input type="file" name="photo" id="fileSelect">
+        <input type="submit" name="submit" value="Upload">
+        <p><strong>Note:</strong> Only .jpg, .jpeg, .gif, .png formats allowed to a max size of 5 MB.</p>
+    </form>
+<h2 align="center">Zoznam všetkých podaných podnetov</h2><br>
+</br>
+Meno a priezvisko: <td><input type='text' name='meno' value='' size='30' required></td><br>
+</br>
+Adresa: <td><input type='text' name='meno' value='' size='30' required></td><br>
+</br>
+
+Obrázok:
+
+        <label "for="fileSelect" ></label>
+        <input type="file" name="photo" id="fileSelect">
+        <input type="submit" name="submit" value="Upload">
+        <p><strong>Note:</strong> Only .jpg, .jpeg, .gif, .png formats allowed to a max size of 5 MB.</p>
+        
+<input type="submit" name="submit" value="Podnet">
+
+<h2 align="center">Zoznam všetkých podaných zoradených podnetov</h2><br>
+Meno a priezvisko: <td><input type='text' name='meno' value='' size='30' required></td>
+Adresa: <td><input type='text' name='meno' value='' size='30' required></td> 
+Obrázok:
+        <label "for="fileSelect" ></label>
+        <input type="file" name="photo" id="fileSelect"></td>
+Dátum podnetu:  <td><input type='text' name='meno' value='' size='30' required></td><br></br>
+
+
+
+Popis podnetu: <td></label><div class='col-xs-10 col-sm-6 col-lg-4'><textarea  name="email_body" cols="35" rows="5"  id="idSendCVemailxbody" class="form-control" ></textarea></div> <span class="data-popover form-info " data-toggle="popover" data-placement="top" data-content="Sprievodn&#xFD;&#x20;text&#x20;sl&#xFA;&#x017E;i&#x20;rovnako&#x20;ako&#x20;e-mail&#x20;na&#x20;oslovenie&#x20;spolo&#x010D;nosti.&#x20;Informujete&#x20;v&#x20;&#x0148;om&#x20;adres&#xE1;ta,&#x20;na&#x20;ak&#xFA;&#x20;poz&#xED;ciu&#x20;reagujete&#x20;a&#x20;ak&#xE9;&#x20;dokumenty&#x20;prip&#xE1;jate.&#x20;M&#xF4;&#x017E;ete&#x20;ho&#x20;vyu&#x017E;i&#x0165;&#x20;aj&#x20;namiesto&#x20;motiva&#x010D;n&#xE9;ho&#x20;listu,&#x20;kde&#x20;uvediete,&#x20;pre&#x010D;o&#x20;V&#xE1;s&#x20;pracovn&#xE1;&#x20;ponuka&#x20;zaujala,&#x20;ak&#xE9;&#x20;m&#xE1;te&#x20;s&#x20;danou&#x20;poz&#xED;ciou&#x20;sk&#xFA;senosti,&#x20;na&#x20;akej&#x20;&#xFA;rovni&#x20;ovl&#xE1;date&#x20;po&#x017E;adovan&#xE9;&#x20;zru&#x010D;nosti&#x20;a&#x20;&#x010D;o&#x20;m&#xF4;&#x017E;ete&#x20;spolo&#x010D;nosti&#x20;pon&#xFA;knu&#x0165;.&#x0A;&lt;br&#x20;&#x2F;&gt;&lt;br&#x20;&#x2F;&gt;&#x0A;V&#x0161;etky&#x20;tipy&#x20;k&#x20;tvorbe&#x20;motiva&#x010D;n&#xE9;ho&#x20;listu&#x20;a&#x20;&#x017E;</td></br>
+Dátum podnetu:  <td><input type='text' name='meno' value='' size='30' required></td><br>
+</br>
+
+</body>
+</html>
+<script>
+
+document.write("A toto napsal JavaScript");
+
+</script>
+</body>
+
---
+++ tlascidlo.js
@@ -0,0 +1,11 @@
+[
+"20140708_____AtestY.cm5",
+"20140711_____Ctestyy.cm5",
+"20140711_____Dtestzz.cm5",
+"20140711_____SBtestyy.cm5"
+]
+
+arrayFileData.sort(function(a,b){
+return a.substr(0,∞) - b.substr(0,∞); // you could swap a and b here
+// to get the array sorted in reverse order
+})
